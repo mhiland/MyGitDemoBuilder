b61ec79cc5518f287edd2273e9db97d0fe894e86
Michael Hiland <mhiland@users.noreply.github.com>
2024-03-02 10:42:48 -0800

    Update to dotnet 8

--- GitDemo/Properties/AssemblyInfo.cs
+++ GitDemo/Properties/AssemblyInfo.cs
@@ -1,9 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GitDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: ComVisible(false)]
 [assembly: Guid("89d6db09-f4de-441f-9132-44f3c56dd4f2")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
